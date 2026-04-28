c1a60cf0e0db088372fe5b417c6db0b64a8fb796
drissi-kl <drissiabderrahmane999@gmail.com>
2026-04-28 23:37:02 +0100

    update slider

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "../../style/slider.css";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Slider() {
-  return <main className='bg-amber-300 px-[200px] my-5 h-[400px]'>
-    <div className='relative'>
-        <div className='absolute w-[200px] top-20 left-20 font-semibold text-3xl'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        </div>
-        <img src="banner_Hero1.jpg" alt="" className='object' />
-    </div>
+  return <main >
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
 
   </main>
 }
